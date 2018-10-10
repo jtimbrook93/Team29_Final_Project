d71526afa3b47d0bc2f369f09afd8afe56b9dec7
jtimbrook93 <jtimbroo@iu.edu>
2018-10-10 11:58:26 -0400

    data post?

--- public/api/dataPost.php
+++ public/api/dataPost.php
@@ -5,3 +5,15 @@ $client = new Client($_POST);
 $client->create();
 
 echo json_encode($client);
+
+$sensor = new Sensor($_POST);
+
+$sensor->create();
+
+echo json_encode($sensor);
+
+$site = new Site($_POST);
+
+$site->create();
+
+echo json_encode($site);
