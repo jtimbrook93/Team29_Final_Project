9afd7af530640909f60e2cf52d09275e77aaa446
jtimbrook93 <jtimbroo@iu.edu>
2018-10-10 17:35:30 -0400

    create base turbine page

--- app/models/Turbine.php
+++ app/models/Turbine.php
@@ -1,4 +1,56 @@
 <?php
 
 class Turbine
-{}
+{
+    public $sensorId;
+    public $sensorName;
+    public $sensorDescription;
+    public $manufacturer;
+    public $totalLifeExpectancyHours;
+    public $sensorDeployedId;
+    public $turbineDeployedId;
+    public $serialNumber;
+    public $deployedDate;
+
+    public function __construct($data) {
+     // creating a new object instance using 'id' as integer
+
+      $this->sensorId = intval($data['sensorId']);
+      $this->sensorName = ($data['sensorName']);
+      $this->sensorDescription = ($data['sensorDescription']);
+      $this->manufacturer = ($data['manufacturer']);
+      $this->totalLifeExpectancyHours = intval($data['totalLifeExpectancyHours']);
+      $this->$sensorDeployedId = intval($data['sensorDeployedId']);
+      $this->$turbineDeployedId = intval($data['turbineDeployedId']);
+      $this->$serialNumber = intval($data['serialNumber']);
+      $this->$deployedDate = ($data['deployedDate']);
+
+    }
+    public function getAllSensors() {
+
+      // 1. Connect to the database
+      $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+
+      // 2. Prepare the query
+      $sql = 'SELECT * FROM Sensor, Sensor_deploy
+            where Sensor.sensorId = Sensor_deploy.sensorId';
+      $statement = $db->prepare($sql);
+
+      // 3. Run the query
+      $success = $statement->execute();
+
+      // 4. Handle the results
+      $arr = [];
+      while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+
+        // 4.a. For each row, make a new work php object
+        $sensorItem =  new Sensor($row);
+        array_push($arr, $sensorItem);
+
+      }
+
+      // 4.b. return the array of work objects
+      return $arr;
+    }
+  
+    }
