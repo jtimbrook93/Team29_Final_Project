69ca43397b3a2a4fef8906e320776aa7d14f3f95
jtimbrook93 <jtimbroo@iu.edu>
2018-10-11 14:12:28 -0400

    adding comment page

--- public/api/dataPost.php
+++ public/api/dataPost.php
@@ -5,15 +5,3 @@ $client = new Client($_POST);
 $client->create();
 
 echo json_encode($client);
-
-$sensor = new Sensor($_POST);
-
-$sensor->create();
-
-echo json_encode($sensor);
-
-$site = new Site($_POST);
-
-$site->create();
-
-echo json_encode($site);
