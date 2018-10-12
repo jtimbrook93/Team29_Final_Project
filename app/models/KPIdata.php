22a7e6c1b160aeb3ebcf19125d06b2fe0854ca33
jtimbrook93 <jtimbroo@iu.edu>
2018-10-11 21:19:49 -0400

    comment line 45

--- app/models/KPIdata.php
+++ app/models/KPIdata.php
@@ -42,7 +42,7 @@ class KPIdata
       $sql = 'SELECT turbineDeployedId, Time_Series_for_KPI.sensorDeployedId, dataCollectedDate,output, heartRate, compressorEfficiency, availability, reliability, firedHours, trips, starts
               FROM Time_Series_for_KPI
               INNER JOIN Sensor_deploy
-              ON Time_Series_for_KPI.sensorDeployedId = Sensor_deploy.sensorDeployedId';
+              ON Time_Series_for_KPI.sensorDeployedId = Sensor_deploy.sensorDeployedId'; //this needs to be fixed eventially
 
       $statement = $db->prepare($sql);
 
