3bca1966db4d8ea1dca485d6bfd5bcdf86c54eef
jtimbrook93 <jtimbroo@iu.edu>
2018-10-25 15:21:36 -0400

    uncaps lock avg

--- app/models/KPI.php
+++ app/models/KPI.php
@@ -38,7 +38,7 @@ class KPI
       $db = new PDO(DB_SERVER, DB_USER, DB_PW);
 
       // 2. Prepare the query
-      $sql = 'SELECT Turbine_deploy.turbineDeployedId, Time_Series_for_KPI.sensorDeployedId, Time_Series_for_KPI.dataCollectedDate, AVG(output), avg(heartRate), avg(compressorEfficiency),
+      $sql = 'SELECT Turbine_deploy.turbineDeployedId, Time_Series_for_KPI.sensorDeployedId, Time_Series_for_KPI.dataCollectedDate, avg(output), avg(heartRate), avg(compressorEfficiency),
               avg(availability), avg(reliability), avg(firedHours), avg(trips), avg(starts)
               from Time_Series_for_KPI, Sensor_deploy, Turbine_deploy
               where Time_Series_for_KPI.sensorDeployedId = Sensor_deploy.sensorDeployedId and Sensor_deploy.turbineDeployedId = Turbine_deploy.turbineDeployedId
