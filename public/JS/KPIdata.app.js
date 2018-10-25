2e322b2cab8c309b82bfe273681101775e1144ad
Ontick Khan <onkhan@iu.edu>
2018-10-25 15:42:23 -0400

    editing sql query for KPI data and site

--- public/JS/KPIdata.app.js
+++ public/JS/KPIdata.app.js
@@ -1,128 +1,50 @@
-var App = new Vue({
-  el: '#Metrics',
-  data: {
-    task: {
-      id: 0,
-      title: 'foo',
-      type : '',
-      size : '',
-      team : '',
-      status: '',
-      start_date: '',
-      close_date: null,
-      hours_worked: '',
-      perc_complete: '',
-      current_sprint : ''
-    },
-    work: [ ],
-    workForm: { },   // populated by this.getEmptyWorkForm()
-    teamList: [] // All the teams
+var metricsApp = new Vue({
+  el: '#metricscontainer',
+data: {
+  metrics: {
+    sensorDeployedId: '',
+    turbineDeployedId: '',
+    dataCollectedDate: '',
+    output: '',
+    heartRate: '',
+    compressorEfficiency: '',
+    availability: '',
+    reliability: '',
+    firedHours: '',
+    trips: '',
+    starts: '',
+}
+
+},
+computed: {
+
   },
-  computed: {
-    workSpan () {
-      return moment(this.workForm.stop + ' ' + this.workForm.stop_time)
-             .diff(moment(this.workForm.start + ' ' + this.workForm.start_time), 'hours', true)
-             .toFixed(1);
-    }
-  },
+
   methods: {
-    handleWorkForm(e) {
+    fetchMetrics(){
+      fetch('api/KPI.php')
+      .then( response => response.json() )  // "a => expression" is shorthand function declaration
+    .then( json => {
+      metricsApp.metrics = json;
+      //  TODO: Build out client chart
 
-
-      // TODO: Check validity
-      if (this.workSpan <= 0) {
-        console.error('Cannot submit, invalid values');
-        return;
-      }
-
-      this.workForm.start_date = this.workForm.start + ' ' + this.workForm.start_time;
-      this.workForm.hours = this.workSpan;
-      //stop field is not used by API
-      //this.workForm.stop_date = this.workForm.stop + ' ' + this.workForm.stop_time;
-
-      const s = JSON.stringify(this.workForm);
-
-      console.log(s);
-
-      //TODO: POST to remote server
-      fetch('api/work.php', {
-        method: "POST",
-        headers: {
-          "CONTENT_TYPE": "application/json; charset=utf-8",
-        },
-        body: s
-      })
-
-      .then( response => response.json() )
-      .then( json => {this.work.push(json)})
-      .catch( err => {
-        console.error('WORK FETCH ERROR:');
-        console.error(err);
-      })
-
-
-      this.work.push(JSON.parse(s));
-
-      // Reset workForm
-      this.workForm = this.getEmptyWorkForm();
-    },
-    sumHours() {
-      return this.work.reduce( (sum, current) => sum + current.hours, 0 )
-    },
-    diffAsHours() {
-      return 0 //moment().duration(end.diff(startTime)).asHours();
-    },
-    datetimeFormat(d) {
-      d = d || moment();
-      return moment(d).format('YYYY-MM-DD[T]HH:MM');
-    },
-    getEmptyWorkForm() {
-      return {
-        start: moment().format('YYYY-MM-DD'),
-        start_time: moment().format('HH:MM'),
-        stop: moment().format('YYYY-MM-DD'),
-        stop_time: moment().format('HH:MM'),
-        team_id: null,
-        task_id: this.task.id,
-        completion_estimate: 0
-      }
-    },
-    prettyDate(d) {
-      return moment(d).format('YYYY-MM-DD HH:MM')
-    }
+    } )
+    .catch( err => {
+      console.log('METRIC LIST FETCH ERROR:');
+      console.log(err);
+    })
   },
+  },
+
   created () {
 
     // Do data fetch
-    const url = new URL(window.location.href);
-    const taskId = url.searchParams.get('taskId');
-    console.log('Task: '+ taskId);
-    this.task.id = taskId;
-
-    if (!taskId) {
-      //TODO: Error? 404?
-      //e.g., window.location = '404.html';
-    }
-
-    // Populate workForm with default values
-    this.workForm = this.getEmptyWorkForm();
-
-    // TODO: Fetch task-specific data
-    // fetch('api/task?id=4')
-    fetch('api/work.php?taskId='+ taskId)
+    fetch('api/KPI.php')
     .then( response => response.json() )
-    .then( json => {tasksApp.work = json} )
+    .then( json => {metricsApp.metrics = json} )
     .catch( err => {
-      console.error('WORK POST ERROR:');
+      console.error('METRIC FETCH ERROR:');
       console.error(err);
-    })
-
-    fetch('api/team.php')
-    .then( response => response.json() )
-    .then( json => {tasksApp.teamList = json} )
-    .catch( err => {
-      console.log('TEAM LIST ERROR:');
-      console.log(err);
     })
   }
 })
