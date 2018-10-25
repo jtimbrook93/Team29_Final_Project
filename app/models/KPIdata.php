ceff191ee8af983a90b8af6573c95373c3acb867
Ontick Khan <onkhan@iu.edu>
2018-10-25 09:37:17 -0400

    beginnings of KPIdata

--- app/models/KPIdata.php
+++ app/models/KPIdata.php
@@ -40,10 +40,15 @@ class KPIdata
 
       // 2. Prepare the query
       // connect turbine_deploy table and pull in siteID information to connect the tables
-      $sql = 'SELECT turbineDeployedId, Time_Series_for_KPI.sensorDeployedId, dataCollectedDate,output, heartRate, compressorEfficiency, availability, reliability, firedHours, trips, starts
+    //this will show the sites for each kpi for a turbine
+      $sql = 'SELECT Turbine_deploy.siteID, Sensor_deploy.turbineDeployedId, Time_Series_for_KPI.sensorDeployedId, dataCollectedDate,output, heartRate, compressorEfficiency, availability, reliability, firedHours, trips, starts
               FROM Time_Series_for_KPI
               INNER JOIN Sensor_deploy
-              ON Time_Series_for_KPI.sensorDeployedId = Sensor_deploy.sensorDeployedId'; //this needs to be fixed eventually
+              ON Time_Series_for_KPI.sensorDeployedId = Sensor_deploy.sensorDeployedId
+              INNER JOIN Sensor_deploy
+              ON Time_Series_for_KPI.turbineDeployedId = Sensor_deploy.turbineDeployedId
+              INNER JOIN Turbine_deploy
+              ON Sensor_deploy.turbineDeployedId = Turbine_deploy.turbineDeployedId'; //this needs to be fixed eventually
 
       $statement = $db->prepare($sql);
 
