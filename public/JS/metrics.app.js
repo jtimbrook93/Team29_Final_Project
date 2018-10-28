56d974d507aaeb12e33a2a89874bfce541ff3436
jtimbrook93 <jtimbroo@iu.edu>
2018-10-28 12:00:07 -0400

    charting

--- public/JS/metrics.app.js
+++ public/JS/metrics.app.js
@@ -35,161 +35,56 @@ computed: {
   },
   },
   buildMetricChart() {
+    Highcharts.chart('metricChart', {
+              title: {
+                  text: 'KPI Metric Chart'
+              },
+              xAxis: {
+                  type: 'Data'
+              },
+              yAxis: {
+                  title: {
+                      text: 'Turbine ID'
+                  }
+              },
+              legend: {
+                  enabled: false
+              },
+              plotOptions: {
+                  area: {
+                      fillColor: {
+                          linearGradient: {
+                              x1: 0,
+                              y1: 0,
+                              x2: 0,
+                              y2: 1
+                          },
+                          stops: [
+                              [0, Highcharts.getOptions().colors[0]],
+                              [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                          ]
+                      },
+                      marker: {
+                          radius: 2
+                      },
+                      lineWidth: 1,
+                      states: {
+                          hover: {
+                              lineWidth: 1
+                          }
+                      },
+                      threshold: null
+                  }
+              },
 
-        // TODO: figure out what all to remove from this shit
-
-        ['mousemove', 'touchmove', 'touchstart'].forEach(function (eventType) {
-            document.getElementById('chartcontainer').addEventListener(
-                eventType,
-                function (e) {
-                    var chart,
-                        point,
-                        i,
-                        event;
-
-                    for (i = 0; i < Highcharts.charts.length; i = i + 1) {
-                        chart = Highcharts.charts[i];
-                        // Find coordinates within the chart
-                        event = chart.pointer.normalize(e);
-                        // Get the hovered point
-                        point = chart.series[0].searchPoint(event, true);
-
-                        if (point) {
-                            point.highlight(e);
-                        }
-                    }
-                }
-            );
-        });
-
-        /**
-         * Override the reset function, we don't need to hide the tooltips and
-         * crosshairs.
-         */
-        Highcharts.Pointer.prototype.reset = function () {
-            return undefined;
-        };
-
-        /**
-         * Highlight a point by showing tooltip, setting hover state and draw crosshair
-         */
-        Highcharts.Point.prototype.highlight = function (event) {
-            event = this.series.chart.pointer.normalize(event);
-            this.onMouseOver(); // Show the hover marker
-            this.series.chart.tooltip.refresh(this); // Show the tooltip
-            this.series.chart.xAxis[0].drawCrosshair(event, this); // Show the crosshair
-        };
-
-        /**
-         * Synchronize zooming through the setExtremes event handler.
-         */
-        function syncExtremes(e) {
-            var thisChart = this.chart;
-
-            if (e.trigger !== 'syncExtremes') { // Prevent feedback loop
-                Highcharts.each(Highcharts.charts, function (chart) {
-                    if (chart !== thisChart) {
-                        if (chart.xAxis[0].setExtremes) { // It is null while updating
-                            chart.xAxis[0].setExtremes(
-                                e.min,
-                                e.max,
-                                undefined,
-                                false,
-                                { trigger: 'syncExtremes' }
-                            );
-                        }
-                    }
-                });
-            }
-        }
-
-        //TODO: figure out what to change in this to get our KPI Time Series data
-        // Get the data. The contents of the data file can be viewed at
-
-        Highcharts.ajax({
-            url: 'https://cdn.rawgit.com/highcharts/highcharts/057b672172ccc6c08fe7dbb27fc17ebca3f5b770/samples/data/activity.json',
-            dataType: 'text',
-            success: function (activity) {
-
-                activity = JSON.parse(activity);
-                activity.datasets.forEach(function (dataset, i) {
-
-                    // Add X values
-                    dataset.data = Highcharts.map(dataset.data, function (val, j) {
-                        return [activity.xData[j], val];
-                    });
-
-                    var metricChart = document.createElement('metric');
-                    metricChart.className = 'metricChart';
-                    document.getElementById('chartcontainer').appendChild(metricChart);
-
-                    Highcharts.chart('metricChart', {
-                        chart: {
-                            marginLeft: 40, // Keep all charts left aligned
-                            spacingTop: 20,
-                            spacingBottom: 20
-                        },
-                        title: {
-                            text: dataset.name,
-                            align: 'left',
-                            margin: 0,
-                            x: 30
-                        },
-                        credits: {
-                            enabled: false
-                        },
-                        legend: {
-                            enabled: false
-                        },
-                        xAxis: {
-                            crosshair: true,
-                            events: {
-                                setExtremes: syncExtremes
-                            },
-                            labels: {
-                                format: '{value} km'
-                            }
-                        },
-                        yAxis: {
-                            title: {
-                                text: null
-                            }
-                        },
-                        tooltip: {
-                            positioner: function () {
-                                return {
-                                    // right aligned
-                                    x: this.chart.chartWidth - this.label.width,
-                                    y: 10 // align to title
-                                };
-                            },
-                            borderWidth: 0,
-                            backgroundColor: 'none',
-                            pointFormat: '{point.y}',
-                            headerFormat: '',
-                            shadow: false,
-                            style: {
-                                fontSize: '18px'
-                            },
-                            valueDecimals: dataset.valueDecimals
-                        },
-                        series: [{
-                            data: dataset.data,
-                            name: dataset.name,
-                            type: dataset.type,
-                            color: Highcharts.getOptions().colors[i],
-                            fillOpacity: 0.3,
-                            tooltip: {
-                                valueSuffix: ' ' + dataset.unit
-                            }
-                        }]
-                    });
-                });
-            }
-        });
-  },
-
-
+              series: [{
+                  type: 'area',
+                  name: 'Hours (Running Total)',
+                  // Data needs [ [date, num], [date2, num2 ], ... ]
+                  data: this.metrics.map( item => [item.date, item.runningTotalHours] )
+              }]
+          });
+        },
   created () {
 
     // Do data fetch
