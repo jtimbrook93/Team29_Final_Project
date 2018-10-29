09397b3a861843ab1755a7a68f1737f6663bd6eb
GeezyIU <geezyiu@gmail.com>
2018-10-29 14:15:44 -0400

    Create fetch Starts

--- public/JS/metrics.app.js
+++ public/JS/metrics.app.js
@@ -147,6 +147,20 @@ computed: {
     this.buildTripsChart();
   },
 
+  fetchStartsMetrics(){
+    fetch('api/kpi.php')
+    .then( response => response.json() )  // "a => expression" is shorthand function declaration
+    .then( json => {
+    metricsApp.startsArr = json;
+    metricsApp.metricsArr = metricsApp.startsArr; } )
+  .catch( err => {
+    console.log('METRIC LIST FETCH ERROR:');
+    console.log(err);
+  })
+
+    this.formatDate();
+    this.buildStartsChart();
+  },
 
   formatDate() {
       this.metricsArr.forEach(
