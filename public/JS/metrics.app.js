3b76b2d8d4adaf8a09ab790ce933bd659616c956
GeezyIU <geezyiu@gmail.com>
2018-10-29 14:22:09 -0400

    Reliability Chart from Highcharts

--- public/JS/metrics.app.js
+++ public/JS/metrics.app.js
@@ -274,6 +274,58 @@ Highcharts.chart('heartRateChart', {
           });
         },
 
+        buildReliabilityChart() {
+        Highcharts.chart('reliabilityChart', {
+                  title: {
+                      text: 'KPI reliability Chart'
+                  },
+                  xAxis: {
+                      type: 'datetime'
+                  },
+                  yAxis: {
+                      title: {
+                          text: 'Reliability'
+                      }
+                  },
+                  legend: {
+                      enabled: false
+                  },
+                  plotOptions: {
+                      area: {
+                          fillColor: {
+                              linearGradient: {
+                                  x1: 0,
+                                  y1: 0,
+                                  x2: 0,
+                                  y2: 1
+                              },
+                              stops: [
+                                  [0, Highcharts.getOptions().colors[0]],
+                                  [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                              ]
+                          },
+                          marker: {
+                              radius: 2
+                          },
+                          lineWidth: 1,
+                          states: {
+                              hover: {
+                                  lineWidth: 1
+                              }
+                          },
+                          threshold: null
+                      }
+                  },
+
+                  series: [{
+                      type: 'area',
+                      name: 'Sensor Reliability',
+                      // Data needs [ [date, num], [date2, num2 ], ... ]
+                      data: this.reliabilityArr.map( item => [item.dataCollectedDate, item.reliability] )
+                  }]
+              });
+            },
+
 
 buildOutputChart() {
   Highcharts.chart('OutputChart', {
