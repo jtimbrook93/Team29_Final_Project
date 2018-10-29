f881f2adc7c9f841c8e17ceff9d9a3c6b1c6abf8
GeezyIU <geezyiu@gmail.com>
2018-10-29 14:49:50 -0400

    Trips Chart from highcharts

--- public/JS/metrics.app.js
+++ public/JS/metrics.app.js
@@ -378,6 +378,58 @@ Highcharts.chart('firedHoursChart', {
       });
     },
 
+    buildTripsChart() {
+    Highcharts.chart('tripsChart', {
+            title: {
+                text: 'KPI trips Chart'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Trips'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor: {
+                        linearGradient: {
+                            x1: 0,
+                            y1: 0,
+                            x2: 0,
+                            y2: 1
+                        },
+                        stops: [
+                            [0, Highcharts.getOptions().colors[0]],
+                            [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        ]
+                    },
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+
+              series: [{
+                  type: 'area',
+                  name: 'Sensor Trips',
+                  // Data needs [ [date, num], [date2, num2 ], ... ]
+                  data: this.tripsArr.map( item => [item.dataCollectedDate, item.trips] )
+              }]
+          });
+        },
+
 
 buildOutputChart() {
   Highcharts.chart('OutputChart', {
