588222264d360f2adfefc91376355405d471c5f3
GeezyIU <geezyiu@gmail.com>
2018-10-29 14:52:34 -0400

    Formatting

--- public/JS/metrics.app.js
+++ public/JS/metrics.app.js
@@ -430,57 +430,57 @@ Highcharts.chart('firedHoursChart', {
           });
         },
 
-        buildStartsChart() {
-        Highcharts.chart('startsChart', {
-                title: {
-                    text: 'KPI starts Chart'
+buildStartsChart() {
+Highcharts.chart('startsChart', {
+        title: {
+            text: 'KPI starts Chart'
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        yAxis: {
+            title: {
+                text: 'Starts'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
                 },
-                xAxis: {
-                    type: 'datetime'
+                marker: {
+                    radius: 2
                 },
-                yAxis: {
-                    title: {
-                        text: 'Starts'
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
                     }
                 },
-                legend: {
-                    enabled: false
-                },
-                plotOptions: {
-                    area: {
-                        fillColor: {
-                            linearGradient: {
-                                x1: 0,
-                                y1: 0,
-                                x2: 0,
-                                y2: 1
-                            },
-                            stops: [
-                                [0, Highcharts.getOptions().colors[0]],
-                                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                            ]
-                        },
-                        marker: {
-                            radius: 2
-                        },
-                        lineWidth: 1,
-                        states: {
-                            hover: {
-                                lineWidth: 1
-                            }
-                        },
-                        threshold: null
-                    }
-                },
+                threshold: null
+            }
+        },
 
-                  series: [{
-                      type: 'area',
-                      name: 'Sensor Starts',
-                      // Data needs [ [date, num], [date2, num2 ], ... ]
-                      data: this.startsArr.map( item => [item.dataCollectedDate, item.starts] )
-                  }]
-              });
-            },
+          series: [{
+              type: 'area',
+              name: 'Sensor Starts',
+              // Data needs [ [date, num], [date2, num2 ], ... ]
+              data: this.startsArr.map( item => [item.dataCollectedDate, item.starts] )
+          }]
+      });
+    },
 
 buildOutputChart() {
   Highcharts.chart('OutputChart', {
