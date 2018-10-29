ee3690cd5ce6afc3e2e4f918bb7323e2a80e6ac6
GeezyIU <geezyiu@gmail.com>
2018-10-29 14:47:46 -0400

    Firedhours Chart from Highcharts

--- public/JS/metrics.app.js
+++ public/JS/metrics.app.js
@@ -317,14 +317,66 @@ Highcharts.chart('reliabilityChart', {
             }
         },
 
-                  series: [{
-                      type: 'area',
-                      name: 'Sensor Reliability',
-                      // Data needs [ [date, num], [date2, num2 ], ... ]
-                      data: this.reliabilityArr.map( item => [item.dataCollectedDate, item.reliability] )
-                  }]
-              });
-            },
+          series: [{
+              type: 'area',
+              name: 'Sensor Reliability',
+              // Data needs [ [date, num], [date2, num2 ], ... ]
+              data: this.reliabilityArr.map( item => [item.dataCollectedDate, item.reliability] )
+          }]
+      });
+    },
+
+buildFiredHoursChart() {
+Highcharts.chart('firedHoursChart', {
+        title: {
+            text: 'KPI fired hours Chart'
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        yAxis: {
+            title: {
+                text: 'FiredHours'
+            }
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: {
+                        x1: 0,
+                        y1: 0,
+                        x2: 0,
+                        y2: 1
+                    },
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                marker: {
+                    radius: 2
+                },
+                lineWidth: 1,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
+
+          series: [{
+              type: 'area',
+              name: 'Sensor Fired Hours',
+              // Data needs [ [date, num], [date2, num2 ], ... ]
+              data: this.firedHoursArr.map( item => [item.dataCollectedDate, item.firedHours] )
+          }]
+      });
+    },
 
 
 buildOutputChart() {
