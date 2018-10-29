dfb743b62d3694ebf5c62ebf34bdf246b6b82826
GeezyIU <geezyiu@gmail.com>
2018-10-29 14:19:18 -0400

    Availability Chart from Highcharts

--- public/JS/metrics.app.js
+++ public/JS/metrics.app.js
@@ -222,6 +222,59 @@ Highcharts.chart('heartRateChart', {
       });
     },
 
+    buildAvailabilityChart() {
+    Highcharts.chart('availabilityChart', {
+              title: {
+                  text: 'KPI availability Chart'
+              },
+              xAxis: {
+                  type: 'datetime'
+              },
+              yAxis: {
+                  title: {
+                      text: 'Availability'
+                  }
+              },
+              legend: {
+                  enabled: false
+              },
+              plotOptions: {
+                  area: {
+                      fillColor: {
+                          linearGradient: {
+                              x1: 0,
+                              y1: 0,
+                              x2: 0,
+                              y2: 1
+                          },
+                          stops: [
+                              [0, Highcharts.getOptions().colors[0]],
+                              [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                          ]
+                      },
+                      marker: {
+                          radius: 2
+                      },
+                      lineWidth: 1,
+                      states: {
+                          hover: {
+                              lineWidth: 1
+                          }
+                      },
+                      threshold: null
+                  }
+              },
+
+              series: [{
+                  type: 'area',
+                  name: 'Sensor Availability',
+                  // Data needs [ [date, num], [date2, num2 ], ... ]
+                  data: this.availabilityArr.map( item => [item.dataCollectedDate, item.availability] )
+              }]
+          });
+        },
+
+
 buildOutputChart() {
   Highcharts.chart('OutputChart', {
             title: {
