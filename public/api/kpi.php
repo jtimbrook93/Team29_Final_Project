0f651ba59e88562acb91e1deab634dd5a1db1b18
jtimbrook93 <jtimbroo@iu.edu>
2018-10-29 22:35:11 -0400

    think i got it

--- public/api/kpi.php
+++ public/api/kpi.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' ) {
 }
 
 // 1. Go to the database and get all work associated with the $taskId
-$kpiArr = KPI::getKPIs();
+$kpiArr = KPI::getKPIs1();
 
 // 2. Convert to JSON
 $json = json_encode($kpiArr,  JSON_PRETTY_PRINT);
