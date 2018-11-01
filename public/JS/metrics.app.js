ff79f91932a68f4e20745004214bd7b4b5d132bc
Ontick Khan <onkhan@iu.edu>
2018-11-01 12:01:47 -0400

    added 2nd trips chart

--- public/JS/metrics.app.js
+++ public/JS/metrics.app.js
@@ -950,6 +950,57 @@ buildCompressorEfficiencyChart() {
                                                  }]
                                              });
                                            },
+                                           buildtripsChart2() {
+                                           Highcharts.chart('tripsChart2', {
+                                                     title: {
+                                                         text: 'KPI Trips Chart'
+                                                     },
+                                                     xAxis: {
+                                                         type: 'datetime'
+                                                     },
+                                                     yAxis: {
+                                                         title: {
+                                                             text: 'Trips'
+                                                         }
+                                                     },
+                                                     legend: {
+                                                         enabled: false
+                                                     },
+                                                     plotOptions: {
+                                                         area: {
+                                                             fillColor: {
+                                                                 linearGradient: {
+                                                                     x1: 0,
+                                                                     y1: 0,
+                                                                     x2: 0,
+                                                                     y2: 1
+                                                                 },
+                                                                 stops: [
+                                                                     [0, Highcharts.getOptions().colors[0]],
+                                                                     [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                                                                 ]
+                                                             },
+                                                             marker: {
+                                                                 radius: 2
+                                                             },
+                                                             lineWidth: 1,
+                                                             states: {
+                                                                 hover: {
+                                                                     lineWidth: 1
+                                                                 }
+                                                             },
+                                                             threshold: null
+                                                         }
+                                                     },
+
+                                                     series: [{
+                                                         type: 'area',
+                                                         name: 'Trips',
+                                                         // Data needs [ [date, num], [date2, num2 ], ... ]
+                                                         data: this.tripsArr2.map( item => [item.dataCollectedDate, item.trips] )
+                                                     }]
+                                                 });
+                                               },
 },
   created () {
   // fetchOutputMetrics();
