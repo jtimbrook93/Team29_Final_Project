132389fff5fd9bdba85d3695b5ad0a122340f943
Ontick Khan <onkhan@iu.edu>
2018-11-01 12:00:49 -0400

    added more KPI charts

--- public/JS/metrics.app.js
+++ public/JS/metrics.app.js
@@ -797,17 +797,17 @@ buildCompressorEfficiencyChart() {
                                     }]
                                 });
                               },
-                              buildavailabilityChart() {
-                               Highcharts.chart('availabilityChart', {
+                              buildfiredHoursChart() {
+                               Highcharts.chart('firedHoursChart', {
                                          title: {
-                                             text: 'KPI Availability Chart'
+                                             text: 'KPI Fired Hours Chart'
                                          },
                                          xAxis: {
                                              type: 'datetime'
                                          },
                                          yAxis: {
                                              title: {
-                                                 text: 'Availability'
+                                                 text: 'Fired Hours'
                                              }
                                          },
                                          legend: {
@@ -842,13 +842,115 @@ buildCompressorEfficiencyChart() {
 
                                          series: [{
                                              type: 'area',
-                                             name: 'Availability',
+                                             name: 'Fired Hours',
                                              // Data needs [ [date, num], [date2, num2 ], ... ]
-                                             data: this.availabilityArr.map( item => [item.dataCollectedDate, item.availability] )
+                                             data: this.firedHoursArr.map( item => [item.dataCollectedDate, item.firedHours] )
                                          }]
                                      });
-                                   },                                   
-   },
+                                   },
+                                   buildfiredHoursChart2() {
+                                   Highcharts.chart('firedHoursChart2', {
+                                             title: {
+                                                 text: 'KPI Fired Hours Chart'
+                                             },
+                                             xAxis: {
+                                                 type: 'datetime'
+                                             },
+                                             yAxis: {
+                                                 title: {
+                                                     text: 'firedHours'
+                                                 }
+                                             },
+                                             legend: {
+                                                 enabled: false
+                                             },
+                                             plotOptions: {
+                                                 area: {
+                                                     fillColor: {
+                                                         linearGradient: {
+                                                             x1: 0,
+                                                             y1: 0,
+                                                             x2: 0,
+                                                             y2: 1
+                                                         },
+                                                         stops: [
+                                                             [0, Highcharts.getOptions().colors[0]],
+                                                             [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                                                         ]
+                                                     },
+                                                     marker: {
+                                                         radius: 2
+                                                     },
+                                                     lineWidth: 1,
+                                                     states: {
+                                                         hover: {
+                                                             lineWidth: 1
+                                                         }
+                                                     },
+                                                     threshold: null
+                                                 }
+                                             },
+
+                                             series: [{
+                                                 type: 'area',
+                                                 name: 'Fired Hours',
+                                                 // Data needs [ [date, num], [date2, num2 ], ... ]
+                                                 data: this.firedHoursArr2.map( item => [item.dataCollectedDate, item.firedHours] )
+                                             }]
+                                         });
+                                       },
+                                       buildtripsChart() {
+                                       Highcharts.chart('tripsChart', {
+                                                 title: {
+                                                     text: 'KPI Trips Chart'
+                                                 },
+                                                 xAxis: {
+                                                     type: 'datetime'
+                                                 },
+                                                 yAxis: {
+                                                     title: {
+                                                         text: 'Trips'
+                                                     }
+                                                 },
+                                                 legend: {
+                                                     enabled: false
+                                                 },
+                                                 plotOptions: {
+                                                     area: {
+                                                         fillColor: {
+                                                             linearGradient: {
+                                                                 x1: 0,
+                                                                 y1: 0,
+                                                                 x2: 0,
+                                                                 y2: 1
+                                                             },
+                                                             stops: [
+                                                                 [0, Highcharts.getOptions().colors[0]],
+                                                                 [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                                                             ]
+                                                         },
+                                                         marker: {
+                                                             radius: 2
+                                                         },
+                                                         lineWidth: 1,
+                                                         states: {
+                                                             hover: {
+                                                                 lineWidth: 1
+                                                             }
+                                                         },
+                                                         threshold: null
+                                                     }
+                                                 },
+
+                                                 series: [{
+                                                     type: 'area',
+                                                     name: 'Trips',
+                                                     // Data needs [ [date, num], [date2, num2 ], ... ]
+                                                     data: this.tripsArr.map( item => [item.dataCollectedDate, item.trips] )
+                                                 }]
+                                             });
+                                           },
+},
   created () {
   // fetchOutputMetrics();
   // fetchOutputMetrics2();
