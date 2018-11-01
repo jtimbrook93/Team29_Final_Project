1711423cd80304e6a6080edbabc1b471c2a775ad
Ontick Khan <onkhan@iu.edu>
2018-11-01 11:21:54 -0400

    added starts metrics for kpi for charts

--- public/JS/metrics.app.js
+++ public/JS/metrics.app.js
@@ -244,6 +244,34 @@ this.formatDate();
 this.buildtripsChart2();
 },
 
+fetchstartsMetrics(){
+  fetch('api/kpi.php')
+  .then( response => response.json() )  // "a => expression" is shorthand function declaration
+  .then( json => {
+  metricsApp.startsArr = json;
+  metricsApp.metricsArr = metricsApp.startsArr;  } )
+.catch( err => {
+  console.log('METRIC LIST FETCH ERROR:');
+  console.log(err);
+})
+this.formatDate();
+this.buildtripsChart();
+},
+
+fetchstartsMetrics2(){
+  fetch('api/kpi2.php')
+  .then( response => response.json() )  // "a => expression" is shorthand function declaration
+  .then( json => {
+  metricsApp.startsArr2 = json;
+  metricsApp.metricsArr2 = metricsApp.startsArr2;  } )
+.catch( err => {
+  console.log('METRIC LIST FETCH ERROR:');
+  console.log(err);
+})
+this.formatDate();
+this.buildtripsChart();
+},
+
 
 
   formatDate() {
